10428591b453bc820f183b2a14be23e12e179c48
itsDavidev <davidlez2mgk@gmail.com>
2022-04-28 12:43:23 -0400

    ✨  narcisstic number function

---
+++ NarcissticNumber/index.js
@@ -0,0 +1,28 @@
+function NarcissticNumber(num) {
+    let sum = 0
+
+    if (Number.isNaN(num)) return false
+
+    const numStr = num.toString().split('')
+
+    numStr.map(n => {
+        const nMap = Number(n)
+        if (Number.isNaN(nMap)) return false
+        const nMapPow = Math.pow(nMap, numStr.length)
+
+
+        sum += nMapPow
+    })
+
+    if (sum === num) return console.log(`${num} is a Narcisstic Number`)
+
+    return console.log(`${num} is not a Narcisstic Number`)
+
+}
+
+
+
+
+NarcissticNumber(370)
+NarcissticNumber(371)
+NarcissticNumber(1634)
